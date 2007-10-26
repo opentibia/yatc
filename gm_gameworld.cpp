7f293e7b94a3a86c85282dc7c1ea04b3dcfc636d
Ivan Vučica <ivan@vucica.net>
2007-10-26 13:16:51 +0000

    * main.cpp: Numeric keypad works (at least on my laptop) * ui.xcf: Fixed the tiled background's replacement size * gm_gameworld.cpp: Added loading of UI elements sprite * gm_gameworld.cpp: Drawing tiled background upon logging in

--- gm_gameworld.cpp
+++ gm_gameworld.cpp
@@ -1,12 +1,16 @@
 
 #include "gm_gameworld.h"
-
+#include "engine.h"
+#include "options.h"
 GM_Gameworld::GM_Gameworld () {
+	ui = g_engine->createSprite("Tibia.pic", 3);
 }
 
 GM_Gameworld::~GM_Gameworld () {
 }
 
 void GM_Gameworld::renderScene() {
-
+	for (int i=0;i<options.w;i+=96)
+		for (int j=0;j<options.h;j+=96)
+			ui->Blit(i,j,0,0,96,96);
 }
--- gm_gameworld.h
+++ gm_gameworld.h
@@ -2,13 +2,15 @@
 #define __GM_GAMEWORLD_H
 
 #include "gamemode.h"
-
+#include "sprite.h"
 class GM_Gameworld : public GameMode {
 public:
 	GM_Gameworld();
 	~GM_Gameworld();
 
 	void renderScene();
+private:
+	Sprite* ui;
 };
 
 #endif
