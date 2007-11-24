649b15bceae96c7870fe19b798ead93ce716b36b
Ivan Vučica <ivan@vucica.net>
2007-11-24 14:59:49 +0000

    * stubbed sprtool * minor update to pictool

--- tools/pictool/main.c
+++ tools/pictool/main.c
@@ -123,7 +123,7 @@ int writepic(const char* filename, int index, SDL_Surface *s){
 	void *data;
 	int i,j,k;
 	fi = fopen(filename, "rb");
-        fo = fopen("__tmp__.pic","wb+");
+    fo = fopen("__tmp__.pic","wb+");
 
 
 	if (!fi || !fo)
@@ -243,7 +243,7 @@ int readpic (const char* filename, int index, SDL_Surface **sr) {
 				printf("CreateRGBSurface failed: %s\n", SDL_GetError());
 				return -1;
 			}
-			
+
 			magenta = SDL_MapRGB(s->format, 255, 0, 255);
 			SDL_FillRect(s, NULL, magenta);
 
@@ -279,12 +279,15 @@ void show_help() {
 	printf("  pictool picfilename picindex bmpfilename [--tobmp|--topic]\n");
 	printf("  pictool picfilename\n");
 	printf("\n");
-	printf("--help		Shows this help\n");
-	printf("--tobmp		Saves the specified subimage of .PIC into a .BMP file\n");
-	printf("--topic		Saves the .BMP into the specified subimage of .PIC file\n");
+	printf("   --help		Shows this help\n");
+	printf("   --tobmp		Saves the specified subimage of .PIC into a .BMP file\n");
+	printf("   --topic		Saves the .BMP into the specified subimage of .PIC file\n");
 	printf("\n");
 	printf(" When passed only the .PIC filename, the program will output details about\n"
 		" it on standard output.\n");
+	printf("\n");
+	printf(" This program cannot create new .PIC from scratch; a .PIC must already\n"
+        " exist.\n");
 	printf("\n");
 	printf(" This is free software: you are free to change and redistribute it.\n"
 		" There is NO WARRANTY, to the extent permitted by law. \n"
