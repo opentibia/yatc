66fed100221ae2e1a2cf179f208819ac234978ef
Ivan Vučica <ivan@vucica.net>
2007-11-27 11:34:54 +0000

    * added some debug-friendly code for  win32 since SDL seems to send SIGTRAP at extremely odd places * creatureui.cpp: added changes contributed by mrauter, proper but uncolorized rendering of creatures

--- creatureui.cpp
+++ creatureui.cpp
@@ -18,9 +18,12 @@
 // Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //////////////////////////////////////////////////////////////////////
 
+// TODO (ivucica#2#) review code added in 2739 which was written by mrauter, it's probably not 100% the way we want to do things, plus we need colorization
+
 #include "creatureui.h"
 #include "engine.h"
 #include "fassert.h"
+#include "gamecontent/creature.h"
 CreatureUI::CreatureUI() : ThingUI() {
 
 	//m_gfx.insert(m_gfx.end(), g_engine->createSprite("Tibia.spr", Objects::getInstance()->getOutfitType(1)->imageData[0]));
@@ -38,19 +41,42 @@ void CreatureUI::Blit(int x,int y, float scale) const {
 
 
 
-	uint32_t activeframe = m_frame *
+	/*uint32_t activeframe = m_frame *
 							(m_obj->ydiv + m_pos.y % m_obj->ydiv) *
-							(m_obj->xdiv + m_pos.x % m_obj->xdiv);
+							(m_obj->xdiv + m_pos.x % m_obj->xdiv);*/
+
+
+    Creature* n = (Creature*)this;
+
+    uint32_t activeframe = 0;
+
+    if (n->getOutfit().m_looktype != 0) {
+        activeframe = n->getLookDir() * m_obj->height * m_obj->width * m_obj->blendframes;
+    }
+
+
 
 	//for(uint32_t k = 0; k < m_obj->blendframes; ++k){ // note: if it's anything except item, there won't be blendframes...
 	{
+	    uint32_t tframes = m_obj->height * m_obj->width * m_obj->blendframes * m_obj->xdiv;
 		//int k = 0;
 		for(uint32_t i = 0; i < m_obj->height; ++i){
 			for(uint32_t j = 0; j < m_obj->width; ++j){
 
 				//ASSERT(activeframe < m_obj->numsprites);
 
-				m_gfx[activeframe]->Blit(x - j*32, y - i*32);
+				//m_gfx[activeframe]->Blit(x - j*32, y - i*32);
+                m_gfx[activeframe]->Blit(x -8 - j*32, y -8 - i*32);
+                switch(n->getOutfit().m_addons){ // ivucica says: i'm not sure this is correct?
+                case 1:
+                    m_gfx[activeframe+(tframes)]->Blit(x -8 - j*32, y -8 -i*32);
+                case 2:
+                    m_gfx[activeframe+(tframes*2)]->Blit(x -8 - j*32, y -8 -i*32);
+                case 3:
+                    m_gfx[activeframe+(tframes)]->Blit(x -8 - j*32, y -8 -i*32);
+                    m_gfx[activeframe+(tframes*2)]->Blit(x -8 - j*32, y -8 -i*32);
+                }
+
 				activeframe++;
 			}
 		}
@@ -60,7 +86,12 @@ void CreatureUI::Blit(int x,int y, float scale) const {
 }
 
 void CreatureUI::loadOutfit() {
-	m_obj = Objects::getInstance()->getOutfitType(12);
+    Creature* n = (Creature*)this;
+    if (n->getOutfit().m_lookitem != 0) {
+        m_obj = Objects::getInstance()->getItemType(n->getOutfit().m_lookitem);
+    } else {
+       m_obj = Objects::getInstance()->getOutfitType(n->getOutfit().m_looktype);
+    }
 
 	for(uint32_t i = 0; i < m_obj->numsprites; i++){
 		m_gfx.insert(m_gfx.end(), g_engine->createSprite("Tibia.spr", m_obj->imageData[i]));
