13569c2852c9c56933b8f86f7f0fcef20cc4ce4d
Ivan Vučica <ivan@vucica.net>
2008-01-19 20:20:22 +0000

    * fixes to pathfinder in utils.cpp * debianised, not completely but it works. info on building a .deb in debian/Readme.DEBIAN

---
+++ debian/yatc-default.ex
@@ -0,0 +1,10 @@
+# Defaults for yatc initscript
+# sourced by /etc/init.d/yatc
+# installed at /etc/default/yatc by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
