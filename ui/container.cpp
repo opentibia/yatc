f4980ac8d6f36e810ba6733996c805beba624d05
Ivan Vučica <ivan@vucica.net>
2008-09-15 18:51:35 +0000

    * upped version to 0.2.2SVN * moved some stuff out of gm_gameword.cpp and into ui/inventory.cpp and ui/container.cpp * added close button to containers (requires glict 76+)

---
+++ ui/container.cpp
@@ -0,0 +1,131 @@
+#include "container.h"
+#include "../gm_gameworld.h"
+#include "../net/protocolgame.h"
+
+winContainer_t::winContainer_t(Container* _container, uint32_t cid) {
+    container = _container;
+    containerId = cid;
+
+    window.SetCaption(container->getName());
+    // wide enough to display 4 items per row, tall enough to display all rows or three rows, whichever is smalle
+    window.SetWidth(150 + 10); // 10 == for scrollbar
+    window.SetHeight(MIN(4+36*3, 4 + (36*ceil(container->getCapacity()/4.))));
+
+    // close button on titlebar
+    // support added to glict svn 76+
+    // this button needs to be relocated manually
+    #if (GLICT_APIREV >= 76)
+    window.AddTitlebarObject(&closebtn);
+    closebtn.SetCaption("X");
+    closebtn.SetWidth(12);
+    closebtn.SetHeight(12);
+    closebtn.SetPos(150 + 10 - 12, 0);
+    closebtn.SetCustomData(this);
+    closebtn.SetOnClick(OnClose);
+    #else
+    #warning For titlebar objects (such as close buttons) to work properly, you need GLICT APIREV 76+
+    #endif
+
+    // we're adding a winpanel object.
+    // sadly, glictWindow does not have virtualsize handling implemented.
+    // glictPanel does have virtual size; we'll add subpanels into glictPanel
+    // which'll be of the same size as the window itself, and the only object
+    // on the window.
+    window.AddObject(&winpanel);
+    winpanel.SetBGActiveness(false);
+    winpanel.SetPos(0,0);
+    winpanel.SetWidth(150 + 10); // same size as for window
+    winpanel.SetHeight(MIN(4+36*3, 4 + (36*ceil(container->getCapacity()/4.))));
+    // virtual size is always to display all rows
+    winpanel.SetVirtualSize(160, 4 + (36*ceil(container->getCapacity()/4.)));
+
+    for(uint32_t i = 0; i != container->getCapacity(); ++i)
+    {
+        glictPanel* panel = new glictPanel;
+        panel->SetPos(5 + ((i % 4) * 36), 4 + (std::floor(i / 4) * 36));
+        panel->SetWidth(32);
+        panel->SetHeight(32);
+        panel->SetBGColor(.1,.1,.1,1);
+        panel->SetCaption("");
+        panel->SetCustomData(this);
+        panel->SetOnPaint(winContainer_t::containerItemOnPaint);
+        panel->SetOnClick(winContainer_t::containerItemOnClick);
+        panel->SetOnMouseDown(winContainer_t::containerItemOnMouseDown);
+        panel->SetOnMouseUp(winContainer_t::containerItemOnMouseUp);
+
+        panel->SetSkin(&g_skin.inv);
+
+        winpanel.AddObject(panel);
+        pnlItems.push_back(panel);
+    }
+}
+
+void winContainer_t::OnClose(glictPos* pos, glictContainer *caller) {
+    GM_Gameworld *gw = ((GM_Gameworld*)g_game);
+	winContainer_t* window = (winContainer_t*)caller->GetCustomData();
+
+    gw->m_protocol->sendCloseContainer(window->containerId);
+}
+
+void winContainer_t::containerItemOnPaint(glictRect *real, glictRect *clipped, glictContainer *caller)
+{
+	winContainer_t* window = (winContainer_t*)caller->GetCustomData();
+	uint32_t slot_id = window->getSlotId(caller);
+	Item* item = window->container->getItem(slot_id);
+
+	if(item != NULL)
+	{
+		item->Blit((int)real->left, (int)real->top, 1.f);
+	}
+}
+
+void winContainer_t::containerItemOnClick(glictPos *relmousepos, glictContainer* callerclass)
+{
+	winContainer_t* window = (winContainer_t*)callerclass->GetCustomData();
+	uint32_t slot_id = window->getSlotId(callerclass);
+	Item* item = window->container->getItem(slot_id);
+    Position p(0xFFFF, window->containerId | 0x40, slot_id);
+    GM_Gameworld* gameclass = (GM_Gameworld*)g_game;
+
+	if(SDL_GetModState() & KMOD_CTRL && item) {
+		gameclass->m_protocol->sendUseItem(p, item->getID(), slot_id);
+	}
+	else if(SDL_GetModState() & KMOD_SHIFT && item) {
+		GM_Gameworld* gameclass = (GM_Gameworld*)g_game;
+		gameclass->m_protocol->sendLookItem(p, item->getID(), slot_id);
+	}
+}
+
+
+void winContainer_t::containerItemOnMouseUp(glictPos *relmousepos,
+	glictContainer* callerclass)
+{
+    GM_Gameworld *gw = ((GM_Gameworld*)g_game);
+
+	winContainer_t* window = (winContainer_t*)callerclass->GetCustomData();
+    uint32_t slot_id = window->getSlotId(callerclass);
+//	Item* item = window->container->getItem(slot_id);
+	uint32_t id = window->container->getId();
+
+	if (gw->isDragging())
+	{
+		Position dest(0xFFFF, id | 0x40, slot_id);
+	    gw->dragComplete(dest);
+        gw->dismissDrag();
+
+	}
+}
+
+void winContainer_t::containerItemOnMouseDown(glictPos *relmousepos,
+	glictContainer* callerclass)
+{
+    GM_Gameworld *gw = ((GM_Gameworld*)g_game);
+
+    winContainer_t* window = (winContainer_t*)callerclass->GetCustomData();
+    uint32_t slot_id = window->getSlotId(callerclass);
+	uint32_t id = window->container->getId();
+
+    gw->setDragCtr(id, slot_id);
+
+}
+
---
+++ ui/inventory.cpp
@@ -0,0 +1,118 @@
+#include <sstream>
+#include "inventory.h"
+#include "../gm_gameworld.h"
+#include "../gamecontent/enums.h"
+#include "../gamecontent/item.h"
+#include "../gamecontent/inventory.h"
+#include "../net/protocolgame.h"
+void pnlInventory_t::inventoryItemOnPaint(glictRect *real, glictRect *clipped, glictContainer *caller)
+{
+
+	slots_t slot = (slots_t)((glictPanel*)caller - (glictPanel*)caller->GetCustomData() + 1); // customdata stores the address of pnlItem[0], and caller is pnlItem[slot]...
+
+
+	Item* item=Inventory::getInstance().getItem(slot);
+	if (item){
+		item->Blit((int)real->left, (int)real->top, 1.f); // TODO (ivucica#5#) if item is not 32x32, scale appropriately
+	} else {
+		int int_slot;
+		switch (slot) {
+		default:
+		case SLOT_NECKLACE:
+			int_slot = 0;
+			break;
+		case SLOT_HEAD:
+			int_slot = 1;
+			break;
+		case SLOT_BACKPACK:
+			int_slot = 2;
+			break;
+		case SLOT_LEFT:
+			int_slot = 3;
+			break;
+		case SLOT_RIGHT:
+			int_slot = 4;
+			break;
+		case SLOT_ARMOR:
+			int_slot = 5;
+			break;
+		case SLOT_LEGS:
+			int_slot = 6;
+			break;
+		case SLOT_RING:
+			int_slot = 7;
+			break;
+		case SLOT_AMMO:
+			int_slot = 8;
+			break;
+		case SLOT_FEET:
+			int_slot = 9;
+			break;
+		}
+
+		g_engine->getUISprite()->Blit((int)real->left, (int)real->top, 96 + 32 * (int_slot % 5), 32 * (int_slot / 5), 32, 32);
+
+	}
+
+	if (0) {
+		std::stringstream s, count;
+
+		if (item)
+			if (item->getCount()>1)
+				count << (int)item->getCount() << "x";
+
+		s << "Slot " << (int)slot << "\n" << count.str();
+
+
+		g_engine->drawText(s.str().c_str(), "minifont", (int)real->left + 2, (int)real->top + 2, TEXTCOLOR_WHITE);
+	}
+
+}
+void pnlInventory_t::inventoryItemOnClick(glictPos *relmousepos,
+	glictContainer* callerclass)
+{
+    GM_Gameworld* gameclass = (GM_Gameworld*)g_game;
+    slots_t slotid = (slots_t)((glictPanel*)callerclass -
+        (glictPanel*)callerclass->GetCustomData() + 1);
+    Position p(0xFFFF, slotid, 0);
+    if(slotid >= 0 && slotid <= 10)
+    {
+        Item* item = Inventory::getInstance().getItem(slotid);
+        if(SDL_GetModState() & KMOD_CTRL && item)
+            gameclass->m_protocol->sendUseItem(p, item->getID(), 0);
+		else
+		if(SDL_GetModState() & KMOD_SHIFT && item)
+            gameclass->m_protocol->sendLookItem(p, item->getID(), 0);
+	}
+}
+
+void pnlInventory_t::inventoryItemOnMouseUp(glictPos *relmousepos,
+	glictContainer* callerclass)
+{
+    GM_Gameworld *gw = ((GM_Gameworld*)g_game);
+	if (gw->isDragging())
+	{
+        slots_t slotid = (slots_t)((glictPanel*)callerclass -
+			(glictPanel*)callerclass->GetCustomData() + 1);
+		if(slotid >= 0 && slotid <= 10) {
+            Position dest(0xFFFF, slotid, 0);
+            gw->dragComplete(dest);
+		}
+        gw->dismissDrag();
+	}
+}
+
+void pnlInventory_t::inventoryItemOnMouseDown(glictPos *relmousepos,
+	glictContainer* callerclass)
+{
+    GM_Gameworld *gw = ((GM_Gameworld*)g_game);
+
+    slots_t slotid = (slots_t)((glictPanel*)callerclass -
+        (glictPanel*)callerclass->GetCustomData() + 1);
+    if(slotid >= 0 && slotid <= 10) {
+        gw->setDragInv(slotid);
+    } else {
+        gw->setDragInv(SLOT_NONE);
+    }
+}
+
