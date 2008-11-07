2fb9a801043d93b525dead14bf2f3c41656e7b3b
Ivan Vučica <ivan@vucica.net>
2008-11-07 15:54:10 +0000

    * work on gui fetcher

--- tools/fetcher/Data_File_FetcherMain.cpp
+++ tools/fetcher/Data_File_FetcherMain.cpp
@@ -4,7 +4,7 @@
  * Author:    Ivan Vucica (ivucica@gmail.com)
  * Created:   2008-10-27
  * Copyright: Ivan Vucica (http://www.objectnetworks.net/)
- * License:
+ * License:   GNU General Public License v2
  **************************************************************/
 
 #include "Data_File_FetcherMain.h"
@@ -12,6 +12,8 @@
 
 #include <wx/sstream.h>
 #include <wx/protocol/http.h>
+#include <wx/thread.h>
+#include <wx/wfstream.h>
 
 //(*InternalHeaders(Data_File_FetcherDialog)
 #include <wx/settings.h>
@@ -22,13 +24,6 @@
 
 
 
-
-
-// khaotic vars
-bool isdownloading = false;
-wxHTTP *h;
-wxInputStream *h_stream;
-// khaotic vars
 
 
 
@@ -68,18 +63,106 @@ const long Data_File_FetcherDialog::ID_STATICTEXT2 = wxNewId();
 const long Data_File_FetcherDialog::ID_TEXTCTRL1 = wxNewId();
 const long Data_File_FetcherDialog::ID_TEXTCTRL2 = wxNewId();
 const long Data_File_FetcherDialog::ID_BUTTON3 = wxNewId();
+const long Data_File_FetcherDialog::ID_GAUGE1 = wxNewId();
 //*)
+const long Data_File_FetcherDialog::ID_WORKEREVENT = wxNewId();
 
 BEGIN_EVENT_TABLE(Data_File_FetcherDialog,wxDialog)
     //(*EventTable(Data_File_FetcherDialog)
     EVT_IDLE(Data_File_FetcherDialog::OnIdle)
     //*)
+    EVT_MENU(ID_WORKEREVENT, Data_File_FetcherDialog::OnWorkerEvent)
 END_EVENT_TABLE()
+
+
+
+
+// khaotic vars
+bool isdownloading = false;
+
+class FetchThread : public wxThread
+{
+public:
+    FetchThread(Data_File_FetcherDialog *frame) : wxThread() {
+        m_frame = frame;
+        h = new wxHTTP();
+    }
+
+    ExitCode Entry() {
+        printf("Usao u dretvu\n");
+        isdownloading = true;
+
+        if (!h->Connect(_("download.tibia.com"))) {
+            castEvent(2);
+            isdownloading = false;
+            wxThread::Sleep(100);
+            return 0;
+        }
+
+        h_stream = h->GetInputStream("/tibia831.tgz");
+
+        wxFileOutputStream fos("tibia831.tgz");
+        if (!fos.IsOk()){
+            castEvent(3);
+            isdownloading = false;
+            wxThread::Sleep(100);
+            return 0;
+        }
+        do {
+            char buf[1024];
+            if (TestDestroy()) {
+                castEvent(4);
+                isdownloading = false;
+                wxThread::Sleep(100);
+                return 0;
+            }
+            printf(",");fflush(stdout);
+            h_stream->Read(buf, sizeof(buf));
+            printf(".");fflush(stdout);
+            fos.Write(buf, h_stream->LastRead());
+        } while(h_stream->LastRead());
+
+        castEvent(1);
+        wxThread::Sleep(100);
+        isdownloading = false;
+        return 0;
+    }
+
+    int whichEvent() const { return eventtype;}
+
+    void castEvent(int type) {
+        // 1 == done
+        // 2 == failed to connect
+        // 3 == failed to open output file
+        // 4 == terminated by main loop
+        wxCommandEvent event( wxEVT_COMMAND_MENU_SELECTED, Data_File_FetcherDialog::ID_WORKEREVENT );
+        event.SetInt( type );
+
+        // send in a thread-safe way
+        wxPostEvent( m_frame, event );
+
+    }
+private:
+
+    wxHTTP *h;
+    wxInputStream *h_stream;
+    Data_File_FetcherDialog *m_frame;
+    wxCriticalSection m_critsect;
+
+    int eventtype;
+
+} * fetchThread;
+
+// end khaotic vars
+
+
+
 
 Data_File_FetcherDialog::Data_File_FetcherDialog(wxWindow* parent,wxWindowID id)
 {
     //(*Initialize(Data_File_FetcherDialog)
     wxBoxSizer* BoxSizer4;
+    wxBoxSizer* BoxSizer5;
     wxBoxSizer* BoxSizer3;
 
     Create(parent, id, _("wxWidgets app"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("id"));
@@ -94,24 +177,28 @@ Data_File_FetcherDialog::Data_File_FetcherDialog(wxWindow* parent,wxWindowID id)
     BoxSizer3->Add(StaticText1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 10);
     BoxSizer2 = new wxBoxSizer(wxVERTICAL);
     Button1 = new wxButton(this, ID_BUTTON1, _("About"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1"));
-    BoxSizer2->Add(Button1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
-    StaticLine1 = new wxStaticLine(this, ID_STATICLINE1, wxDefaultPosition, wxSize(10,-1), wxLI_HORIZONTAL, _T("ID_STATICLINE1"));
+    BoxSizer2->Add(Button1, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
+    StaticLine1 = new wxStaticLine(this, ID_STATICLINE1, wxDefaultPosition, wxSize(245,2), wxLI_HORIZONTAL, _T("ID_STATICLINE1"));
     BoxSizer2->Add(StaticLine1, 0, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
     Button2 = new wxButton(this, ID_BUTTON2, _("Quit"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
-    BoxSizer2->Add(Button2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
-    BoxSizer3->Add(BoxSizer2, 0, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
-    BoxSizer1->Add(BoxSizer3, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-    BoxSizer4 = new wxBoxSizer(wxHORIZONTAL);
+    BoxSizer2->Add(Button2, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
+    BoxSizer3->Add(BoxSizer2, 0, wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 4);
+    BoxSizer1->Add(BoxSizer3, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer4 = new wxBoxSizer(wxVERTICAL);
+    BoxSizer5 = new wxBoxSizer(wxHORIZONTAL);
     StaticText2 = new wxStaticText(this, ID_STATICTEXT2, _("Type in latest version:"), wxDefaultPosition, wxDefaultSize, 0, _T("ID_STATICTEXT2"));
-    BoxSizer4->Add(StaticText2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer5->Add(StaticText2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
     TextCtrl1 = new wxTextCtrl(this, ID_TEXTCTRL1, _("8"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL1"));
     TextCtrl1->SetMaxLength(1);
-    BoxSizer4->Add(TextCtrl1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer5->Add(TextCtrl1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
     TextCtrl2 = new wxTextCtrl(this, ID_TEXTCTRL2, _("31"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_TEXTCTRL2"));
     TextCtrl2->SetMaxLength(2);
-    BoxSizer4->Add(TextCtrl2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer5->Add(TextCtrl2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
     Button3 = new wxButton(this, ID_BUTTON3, _("Download"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON3"));
-    BoxSizer4->Add(Button3, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer5->Add(Button3, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer4->Add(BoxSizer5, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    Gauge1 = new wxGauge(this, ID_GAUGE1, 100, wxDefaultPosition, wxDLG_UNIT(this,wxSize(303,27)), 0, wxDefaultValidator, _T("ID_GAUGE1"));
+    BoxSizer4->Add(Gauge1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
     BoxSizer1->Add(BoxSizer4, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
     SetSizer(BoxSizer1);
     BoxSizer1->SetSizeHints(this);
@@ -130,6 +217,8 @@ Data_File_FetcherDialog::~Data_File_FetcherDialog()
 
 void Data_File_FetcherDialog::OnQuit(wxCommandEvent& event)
 {
+    if (isdownloading)
+        fetchThread->Delete();
     Close();
 }
 
@@ -141,14 +230,15 @@ void Data_File_FetcherDialog::OnAbout(wxCommandEvent& event)
 
 void Data_File_FetcherDialog::OnButton3Click(wxCommandEvent& event)
 {
-    h = new wxHTTP();
-
-    isdownloading=true;
-
+    fetchThread = new FetchThread(this);
+    fetchThread->Create();
+    fetchThread->Run();
+    printf("Pokrenuta dretva\n");
 }
 
 void Data_File_FetcherDialog::OnIdle(wxIdleEvent& event)
 {
+    /*
     if (isdownloading){
         wxStringOutputStream sos;
         if (h_stream){
@@ -166,4 +256,10 @@ void Data_File_FetcherDialog::OnIdle(wxIdleEvent& event)
         //delete h;
 
     }
+    */
+}
+
+void Data_File_FetcherDialog::OnWorkerEvent(wxCommandEvent& event){
+    printf("Received a message\n");
+    printf("%d\n", event.GetInt());
 }
