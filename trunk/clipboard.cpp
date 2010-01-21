a3644e8e4cd9fb2d4eaccafddcedfec5791fabae
Nate Fries <ntspace@gmail.com>
2010-01-21 15:57:24 +0000

    * Lifted some code by SDL's developer (with his permission) for an X11 clipboard. Modified it a bit to be smaller and more like the pre-existing coding style in YATC. Not tested, so it may or may not work. * Changed Unix builds to denote that they will now only work with X11, just in case someone interested in YATC uses some other system for their GUI * Makefile/configure needs to be updated for Unix with X11. I'm no good with this stuff, so I'll leave it to ivucica.

--- trunk/clipboard.cpp
+++ trunk/clipboard.cpp
@@ -28,12 +28,42 @@
 #warning No clue whether or not Mac OS X clipboard works as of yet.
 #endif
 
+#ifdef USE_X11_CLIPBOARD
+// Important note: X11 clipboard code is lifted and modified from SDL_scrap (by Sam Lantinga) with his explicit permission.
+#include <SDL/SDL.h>
+#include <SDL/SDL_syswm.h>
+#endif
+
 #include "clipboard.h"
 
 yatcClipboard::yatcClipboard()
 {
 	#ifdef __USE_INTERNAL_CLIPBOARD
 	m_text = "";
+	#elifdef USE_X11_CLIPBOARD
+	SDL_SysWMinfo info;
+	SDL_VERSION(&info.version);
+    if(SDL_GetWMInfo(&info))
+    {
+        /* Save the information for later use */
+        if (info.subsystem == SDL_SYSWM_X11)
+        {
+            m_display = info.info.x11.display;
+            m_window = info.info.x11.window;
+            m_lockdisplay = info.info.x11.lock_func;
+            m_unlockdisplay = info.info.x11.unlock_func;
+
+            /* Enable the special window hook events */
+            SDL_EventState(SDL_SYSWMEVENT, SDL_ENABLE);
+            SDL_SetEventFilter(clipboard_filter);
+        }
+        else
+        {
+            NativeGUIError(("SDL is not running on X11!\n"
+                            "Please undefine USE_X11_CLIPBOARD\n"
+                            "and rebuild " PRODUCTSHORT "."), "Clipboard Failure");
+        }
+    }
 	#endif
 }
 yatcClipboard::~yatcClipboard()
@@ -52,6 +82,61 @@ std::string yatcClipboard::getText()
 	return ret;
 	#elif defined (__USE_INTERNAL_CLIPBOARD)
 	return m_text;
+	#elif defined(USE_X11_CLIPBOARD)
+    Window owner;
+    Atom selection;
+    Atom seln_type;
+    int seln_format;
+    unsigned long nbytes;
+    unsigned long overflow;
+    char *src;
+
+    m_lockdisplay();
+    owner = XGetSelectionOwner(m_display, XA_PRIMARY);
+    m_unlockdisplay();
+    if((owner == None) || (owner == m_window))
+    {
+        owner = DefaultRootWindow(m_display);
+        selection = XA_CUT_BUFFER0;
+    }
+    else
+    {
+        int selection_response = 0;
+        SDL_Event event;
+
+        owner = m_window;
+        m_lockdisplay();
+        selection = XInternAtom(m_display, "SDL_SELECTION", False);
+        XConvertSelection(m_display, XA_PRIMARY, XA_STRING,
+                                        selection, owner, CurrentTime);
+        m_unlockdisplay();
+        // Question to ask Sam about this part of code: Is it proper to just drop any events
+        // that are unfortunately before the selection event?
+        while (!selection_response)
+        {
+            SDL_WaitEvent(&event);
+            if(event.type == SDL_SYSWMEVENT)
+            {
+                XEvent xevent = event.syswm.msg->event.xevent;
+
+                if((xevent.type == SelectionNotify) &&
+                     (xevent.xselection.requestor == owner))
+                    selection_response = 1;
+            }
+        }
+    }
+    m_lockdisplay();
+    if(XGetWindowProperty(m_display, owner, selection, 0, INT_MAX/4,
+                            False, XA_STRING, &seln_type, &seln_format,
+                       &nbytes, &overflow, (unsigned char **)&src) == Success)
+    {
+        if (seln_type == XA_STRING)
+        {
+            return std::string(src);
+        }
+        XFree(src);
+    }
+    m_unlockdisplay();
 	#elif defined(__APPLE__)
 	const char* ret = getPasteboardText();
 	std::string str = ret;
@@ -77,7 +162,71 @@ void yatcClipboard::setText(const std::string& text)
 	CloseClipboard();
 	#elif defined (__USE_INTERNAL_CLIPBOARD)
 	m_text = text;
+	#elif defined(USE_X11_CLIPBOARD)
+	m_lockdisplay();
+    XChangeProperty(m_display, DefaultRootWindow(m_display),
+        XA_CUT_BUFFER0, XA_STRING, 8, PropModeReplace, src, srclen);
+    if (lost_scrap())
+        XSetSelectionOwner(m_display, XA_PRIMARY, m_window, CurrentTime);
+    m_unlockdisplay();
 	#elif defined(__APPLE__)
 	putPasteboardText(text.c_str());
 	#endif
 }
+
+#ifdef USE_X11_CLIPBOARD
+int clipboard_filter(const SDL_Event *event)
+{
+    /* Post all non-window manager specific events */
+    if(event->type != SDL_SYSWMEVENT) {
+        return(1);
+    }
+
+    /* Handle window-manager specific clipboard events */
+    switch (event->syswm.msg->event.xevent.type) {
+        /* Copy the selection from XA_CUT_BUFFER0 to the requested property */
+        case SelectionRequest: {
+            XSelectionRequestEvent *req;
+            XEvent sevent;
+            int seln_format;
+            unsigned long nbytes;
+            unsigned long overflow;
+            unsigned char *seln_data;
+
+            req = &event->syswm.msg->event.xevent.xselectionrequest;
+            sevent.xselection.type = SelectionNotify;
+            sevent.xselection.display = req->display;
+            sevent.xselection.selection = req->selection;
+            sevent.xselection.target = None;
+            sevent.xselection.property = None;
+            sevent.xselection.requestor = req->requestor;
+            sevent.xselection.time = req->time;
+            if(XGetWindowProperty(m_display, DefaultRootWindow(m_display),
+                              XA_CUT_BUFFER0, 0, INT_MAX/4, False, req->target,
+                              &sevent.xselection.target, &seln_format,
+                              &nbytes, &overflow, &seln_data) == Success)
+            {
+                if (sevent.xselection.target == req->target)
+                {
+                    if (sevent.xselection.target == XA_STRING)
+                    {
+                        if (seln_data[nbytes-1] == '\0')
+                            --nbytes;
+                    }
+                    XChangeProperty(m_display, req->requestor, req->property,
+                        sevent.xselection.target, seln_format, PropModeReplace,
+                                                      seln_data, nbytes);
+                    sevent.xselection.property = req->property;
+                }
+                XFree(seln_data);
+            }
+            XSendEvent(m_display,req->requestor,False,0,&sevent);
+            XSync(m_display, False);
+        }
+        break;
+    }
+
+    /* Post the event for X11 clipboard reading above */
+    return(1);
+}
+#endif
--- trunk/clipboard.h
+++ trunk/clipboard.h
@@ -23,6 +23,8 @@
 #ifdef __APPLE__
     extern "C" const char* getPasteboardText();
     extern "C" void putPasteBoardText(const char*);
+#elif defined(USE_X11_CLIPBOARD)
+	#include <SDL/SDL_events.h>
 #elif !defined(WIN32)
 	// NOTE (nfries88):
 	//		No X11 clipboard currently, X11 clipboard seems a PITA to implement.
@@ -44,5 +46,12 @@ public:
 private:
 	#ifdef __USE_INTERNAL_CLIPBOARD
 	std::string m_text;
+	#elif defined(USE_X11_CLIPBOARD)
+    Display *m_display;
+    Window m_window;
+    void (*m_lockdisplay)(void);
+    void (*m_unlockdisplay)(void);
+
+    static int clipboard_filter(const SDL_Event *event);
 	#endif
 };
