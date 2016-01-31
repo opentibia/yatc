55d8a6bdd453a293e5d8f3546b8a077e3b5ba458
Ivan Vučica <ivan@vucica.net>
2016-01-31 13:23:19 +0100

    Bitrot cleanup: Include stdlib.h for free().

--- clipboard.cpp
+++ clipboard.cpp
@@ -18,6 +18,8 @@
 // Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //////////////////////////////////////////////////////////////////////
 
+#include <stdlib.h>
+
 #ifdef WIN32
 #include <windows.h>
 
