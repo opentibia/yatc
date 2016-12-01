7bb10e0f11629b78179a8efad1f06c3646c52934
Ivan Vučica <ivan@vucica.net>
2016-12-01 21:57:34 +0000

    We can refer to EngineGL only on builds which USE_OPENGL.
    Affects only builds which do not USE_OPENGL.

--- sprite.cpp
+++ sprite.cpp
@@ -421,9 +421,11 @@ void Sprite::templatedColorize(Sprite* templatespr, uint8_t head, uint8_t body, 
 		printf("!m_image\n");
 		return;
 	}
+	#ifdef USE_OPENGL
 	if(dynamic_cast<EngineGL*>(g_engine))
 		// gl engine is crashing on SDL_UnlockSurface.
 		return;
+	#endif
 	templatespr->lockSurface();
 	if(SDL_MUSTLOCK(m_image)) SDL_LockSurface(m_image);
 	for(int i=0; i < m_image->h; i++){
