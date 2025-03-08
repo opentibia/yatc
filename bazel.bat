e1e25457459576fdf9e19f6d0f94e79a65441c04
Ivan Vučica <ivan@vucica.net>
2025-03-08 18:33:44 +0000

    bazel-win32: Update bazel.bat to point to the paths present on author's local system, since attempt to build tries to use VC2022.

--- bazel.bat
+++ bazel.bat
@@ -1,2 +1,15 @@
-set BAZEL_VC=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC
+set BAZEL_VC=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC
+set BAZEL_VC_FULL_VERSION=14.16.27023
+
+rem Windows SDK in C:\Program Files (x86)\Windows Kits\10
+set BAZEL_WINSDK_FULL_VERSION=10.0.22621.0
+
+rem Architectures:
+rem x64: --cpu=x64_windows
+rem x86: --cpu=x64_x86_windows
+rem arm: --cpu=x64_arm_windows
+rem arm: --cpu=arm64_windows <- deprecated x64_arm64_windows
+
+rem Not using BAZEL_LLVM, as it'd have to use a MSVC-compatible compiler driver (clang-cl.exe) which might not be shipping with above.
+
 bazelisk-windows-amd64.exe %*
